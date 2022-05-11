c40a1d7216e24d7de81eaca1c8c09f3becf237ef
Mike Szczys <mike@golioth.io>
2022-05-11 10:25:19 -0500

    Added sample: hello_ethernet
    This sample adds Ethernet support to the Golioth Hello sample.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ hello-ethernet/src/main.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2021 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(golioth_hello, LOG_LEVEL_DBG);
+
+#include <net/coap.h>
+#include <net/golioth/system_client.h>
+#include <net/golioth/wifi.h>
+
+#if IS_ENABLED(CONFIG_NET_L2_ETHERNET)
+#include <net/net_if.h>
+#endif
+
+static struct golioth_client *client = GOLIOTH_SYSTEM_CLIENT_GET();
+
+static void golioth_on_message(struct golioth_client *client,
+			       struct coap_packet *rx)
+{
+	uint16_t payload_len;
+	const uint8_t *payload;
+	uint8_t type;
+
+	type = coap_header_get_type(rx);
+	payload = coap_packet_get_payload(rx, &payload_len);
+
+	if (!IS_ENABLED(CONFIG_LOG_BACKEND_GOLIOTH) && payload) {
+		LOG_HEXDUMP_DBG(payload, payload_len, "Payload");
+	}
+}
+
+void main(void)
+{
+	int counter = 0;
+	int err;
+
+	LOG_DBG("Start Hello sample");
+
+	if (IS_ENABLED(CONFIG_NET_L2_ETHERNET))
+	{
+		LOG_INF("Connecting to Ethernet");
+		struct net_if *iface;
+		iface = net_if_get_default();
+		net_dhcpv4_start(iface);
+	}
+
+	client->on_message = golioth_on_message;
+	golioth_system_client_start();
+
+	while (true) {
+		LOG_INF("Sending hello! %d", counter);
+
+		err = golioth_send_hello(client);
+		if (err) {
+			LOG_WRN("Failed to send hello!");
+		}
+		++counter;
+		k_sleep(K_SECONDS(5));
+	}
+}
